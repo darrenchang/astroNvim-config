3f1431df90021ae7ce3b6793e6f432dd2d8585e7
Darren <darrenchang951@gmail.com>
2024-01-31 06:04:44 +0000

    Added harpoon and to do comments (with Ryan's love)

---
+++ lua/plugins/user.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    event = "User Astrofile",
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = "VeryLazy",
+  },
+}
